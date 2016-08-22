7edb598f5d0a254de657bceed2090b4d34fa41c2
Juan Jose Jurko <jurkorreo@gmail.com>
2016-08-21 20:35:08 -0500

    Hola mundo label

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,25 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <stdio.h>
+using namespace std;
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setText("Hola Mundo");
 }
